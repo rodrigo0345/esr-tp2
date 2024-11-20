bee1af3f25cc1e1767b27568ae8276b9f2f39c41
Rodrigo <rodrigocralha@gmail.com>
2024-11-20 23:02:46 +0000

    plays video only once

--- client/client.go
+++ client/client.go
@@ -1,151 +1,145 @@
 package client
 
 import (
-    "bytes"
-    "fmt"
-    "image"
-    "image/jpeg"
-    "log"
-    "net"
-    "time"
+	"bytes"
+	"fmt"
+	"image/jpeg"
+	"log"
+	"net"
+	"time"
 
-    "github.com/rodrigo0345/esr-tp2/config"
-    "github.com/rodrigo0345/esr-tp2/config/protobuf" // Import your generated protobuf package
-    "google.golang.org/protobuf/proto"
+	"github.com/rodrigo0345/esr-tp2/config"
+	"github.com/rodrigo0345/esr-tp2/config/protobuf"
+	"google.golang.org/protobuf/proto"
 
-    "fyne.io/fyne/v2"
-    "fyne.io/fyne/v2/app"
-    "fyne.io/fyne/v2/canvas"
-    "fyne.io/fyne/v2/container"
-    "fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func Client(config *config.AppConfigList) {
-    // Address to send messages
-    presenceNetworkEntrypointIp := config.Neighbors[0]
-    pneIpString := fmt.Sprintf("%s:%d", presenceNetworkEntrypointIp.Ip, presenceNetworkEntrypointIp.Port)
-    fmt.Println(pneIpString)
+	// Address to send messages
+	presenceNetworkEntrypointIp := config.Neighbors[0]
+	pneIpString := fmt.Sprintf("%s:%d", presenceNetworkEntrypointIp.Ip, presenceNetworkEntrypointIp.Port)
+	fmt.Println(pneIpString)
 
-    listenIp := "127.0.0.1:2222"
+	listenIp := "0.0.0.0:2222"
 
-    // Setup a UDP connection for sending
-    conn, err := net.Dial("udp", pneIpString)
-    if err != nil {
-        log.Fatalf("Failed to dial: %v", err)
-    }
-    defer conn.Close()
+	// Setup a UDP connection for sending
+	conn, err := net.Dial("udp", pneIpString)
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+	defer conn.Close()
 
-    // Create a UDP listener for incoming messages
-    laddr := net.UDPAddr{
-        Port: 2222,
-        IP:   net.ParseIP("127.0.0.1"),
-    }
-    listener, err := net.ListenUDP("udp", &laddr)
-    if err != nil {
-        log.Fatalf("Failed to create UDP listener: %v", err)
-    }
-    defer listener.Close()
+	// Create a UDP listener for incoming messages
+	laddr := net.UDPAddr{
+		Port: 2222,
+		IP:   net.ParseIP(listenIp),
+	}
 
-    // Create the app and window
-    myApp := app.New()
-    myWindow := myApp.NewWindow("Video Client")
+	listener, err := net.ListenUDP("udp", &laddr)
+	if err != nil {
+		log.Fatalf("Failed to create UDP listener: %v", err)
+	}
 
-    // Create an image canvas object
-    img := canvas.NewImageFromImage(nil)
-    img.FillMode = canvas.ImageFillContain
+	// Create the app and window
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Video Client")
 
-    // Create entry widgets and a button for sending messages
-    messageEntry := widget.NewEntry()
-    messageEntry.SetPlaceHolder("Enter message")
-    targetEntry := widget.NewEntry()
-    targetEntry.SetPlaceHolder("Enter target")
-    sendButton := widget.NewButton("Send", func() {
-        text := messageEntry.Text
-        target := targetEntry.Text
+	// Create an image canvas object
+	imgCanvas := canvas.NewImageFromImage(nil)
+	imgCanvas.FillMode = canvas.ImageFillContain
 
-        message := protobuf.Header{
-            Type:           protobuf.RequestType_RETRANSMIT,
-            Length:         0,
-            Timestamp:      time.Now().UnixMilli(),
-            ClientIp:       listenIp,
-            Sender:         "client",
-            Target:         target,
-            RequestedVideo: "lol.Mjpeg",
-            Content: &protobuf.Header_ClientCommand{
-                ClientCommand: &protobuf.ClientCommand{
-                    Command:               protobuf.PlayerCommand_PLAY,
-                    AdditionalInformation: text,
-                },
-            },
-        }
+	// Create entry widgets and a button for sending messages
+	messageEntry := widget.NewEntry()
+	messageEntry.SetPlaceHolder("Enter message")
+	targetEntry := widget.NewEntry()
+	targetEntry.SetPlaceHolder("Enter target")
+	sendButton := widget.NewButton("Send", func() {
+		text := messageEntry.Text
+		target := targetEntry.Text
 
-        fmt.Printf("Sending message: %s to %s\n", text, target)
-        message.Length = int32(proto.Size(&message))
-        data, err := proto.Marshal(&message)
-        if err != nil {
-            log.Printf("Error marshaling message: %v", err)
-            return
-        }
+		message := protobuf.Header{
+			Type:           protobuf.RequestType_RETRANSMIT,
+			Length:         0,
+			Timestamp:      time.Now().UnixMilli(),
+			ClientIp:       listenIp,
+			Sender:         "client",
+			Target:         target,
+			RequestedVideo: "lol.Mjpeg",
+			Content: &protobuf.Header_ClientCommand{
+				ClientCommand: &protobuf.ClientCommand{
+					Command:               protobuf.PlayerCommand_PLAY,
+					AdditionalInformation: text,
+				},
+			},
+		}
 
-        _, err = conn.Write(data)
-        if err != nil {
-            log.Printf("Failed to send message: %v", err)
-        }
-    })
+		fmt.Printf("Sending message: %s to %s\n", text, target)
+		message.Length = int32(proto.Size(&message))
+		data, err := proto.Marshal(&message)
+		if err != nil {
+			log.Printf("Error marshaling message: %v", err)
+			return
+		}
 
-    controls := container.NewVBox(messageEntry, targetEntry, sendButton)
-    content := container.NewBorder(nil, controls, nil, nil, img)
+		_, err = conn.Write(data)
+		if err != nil {
+			log.Printf("Failed to send message: %v", err)
+		}
+	})
 
-    myWindow.SetContent(content)
-    myWindow.Resize(fyne.NewSize(640, 480))
+	controls := container.NewVBox(messageEntry, targetEntry, sendButton)
+	content := container.NewBorder(nil, controls, nil, nil, imgCanvas)
 
-    // Channel to send images from the goroutine to the main thread
-    imgChan := make(chan image.Image)
+	myWindow.SetContent(content)
+	myWindow.Resize(fyne.NewSize(640, 480))
 
-    // Run a goroutine to listen for incoming messages
-    go func() {
-        for {
-            buffer := make([]byte, 65535)
-            n, _, err := listener.ReadFromUDP(buffer)
-            if err != nil {
-                log.Printf("Error receiving message: %s\n", err)
-                continue
-            }
+	// Run a goroutine to listen for incoming messages
+  fmt.Println("Listening for messages")
+	go func() {
+		for {
+			buffer := make([]byte, 65535)
+			n, _, err := listener.ReadFromUDP(buffer)
 
-            var msg protobuf.Header
-            err = proto.Unmarshal(buffer[:n], &msg)
-            if err != nil {
-                log.Printf("Error unmarshaling message: %s\n", err)
-                continue
-            }
+      fmt.Println(n)
 
-            if msg.GetServerVideoChunk() == nil {
-                continue
-            }
-            videoData := msg.GetServerVideoChunk().Data
+			if err != nil {
+				log.Printf("Error receiving message: %s\n", err)
+				continue
+			}
 
-            // Decode the JPEG data into an image.Image
-            imgReader := bytes.NewReader(videoData)
-            frame, err := jpeg.Decode(imgReader)
-            if err != nil {
-                log.Printf("Error decoding JPEG: %s\n", err)
-                continue
-            }
+			var msg protobuf.Header
+			err = proto.Unmarshal(buffer[:n], &msg)
+			if err != nil {
+				log.Printf("Error unmarshaling message: %s\n", err)
+				continue
+			}
 
-            // Send the image to the main thread via the channel
-            imgChan <- frame
-        }
-    }()
+			if msg.GetServerVideoChunk() == nil {
+				continue
+			}
+			videoData := msg.GetServerVideoChunk().Data
 
-    // In the main thread, receive images from the channel and update the image widget
-        for frame := range imgChan {
-            // Need to update the UI in the main thread
-            fyne.CurrentApp().RunOnMain(func() {
-                img.Image = frame
-                img.Refresh()
-            })
-        }
+			// Decode the JPEG data into an image.Image
+			imgReader := bytes.NewReader(videoData)
+			frame, err := jpeg.Decode(imgReader)
+			if err != nil {
+				log.Printf("Error decoding JPEG: %s\n", err)
+				continue
+			}
 
-    myWindow.ShowAndRun()
+			// Update the image canvas with the new frame
+			frameCopy := frame // Create a copy to avoid concurrency issues
+			imgCanvas.Image = frameCopy
+			imgCanvas.Refresh()
+		}
+	}()
+
+	myWindow.ShowAndRun()
 }
+
 
