731ce106a42a8a536fc35d82e75738a5e2cf8716
rodrigo0345 <rodrigocralha@gmail.com>
2024-11-27 21:18:04 +0000

    stream video

--- client/client.go
+++ client/client.go
@@ -13,11 +13,7 @@ import (
 	"github.com/rodrigo0345/esr-tp2/config/protobuf"
 )
 
-var bsSystem *bootstrapsystem.BootstrapSystem
-var updateUI func(*protobuf.Header) = nil
-
 func Client(cnf *cnf.AppConfigList) {
-
 	var bsAddr string
 	if cnf.Neighbors == nil {
 		bsAddr = "10.0.13.10:3242"
@@ -26,144 +22,136 @@ func Client(cnf *cnf.AppConfigList) {
 	}
 
 	logger := config.NewLogger(2, cnf.NodeName)
-	bsSystem = bootstrapsystem.NewBsSystem(logger, cnf)
+	bsSystem := bootstrapsystem.NewBsSystem(logger, cnf)
 	go bsSystem.ProcessBsCommands()
 
-	listenIp := "0.0.0.0:2222"
 	laddr := net.UDPAddr{
 		Port: 2222,
-		IP:   net.ParseIP(listenIp),
+		IP:   net.IPv4zero, // Listen on all interfaces
 	}
 	listener, err := net.ListenUDP("udp", &laddr)
-
 	if err != nil {
 		log.Fatalf("Failed to create UDP listener: %v", err)
 	}
+	defer listener.Close()
 
+	// Channel to signal when to create the UI
+	createUIChan := make(chan *bootstrapsystem.Pop)
+
+	// Channel for UI updates (buffered to prevent blocking)
+	uiChannel := make(chan *protobuf.Header, 100)
+
+	// Start requesting neighbors
 	go RequestNeighbors(cnf, bsAddr, bsSystem)
 
-	createUI := false
-	var bestPop *bootstrapsystem.Pop = nil
-	// main listener loop
+	// Start the main listener loop
 	go func() {
 		for {
-
 			data, _, err := config.ReceiveMessageUDP(listener)
-
 			if err != nil {
 				log.Printf("Error receiving message: %s\n", err)
 				continue
 			}
 
-			go func(data []byte) {
-
-				msg, err := config.UnmarshalHeader(data)
-				if err != nil {
-					log.Printf("Error unmarshaling message: %s\n", err)
-					return
-				}
-
-				switch msg.Type {
-				case protobuf.RequestType_CLIENT_PING:
-
-					// bootrapper logic
-					// ping all the pops and find the best one
-					if msg.Sender == "bs" {
-
-						logger.Debug("Received Neighbors from bootstraper")
-						cbNb := ProcessNeighbors(cnf, msg, bsSystem)
-
-						select {
-						case result := <-cbNb:
-
-							if result.Success {
-								logger.Debug("Successfully received neighbors from bootstraper")
-								ProcessBestPop(cnf, msg, bsSystem)
-								return
-							}
-
-							logger.Debug("Failed to receive neighbors from bootstraper")
-						}
-						return
-					}
-
-					logger.Debug("Received POP Ping")
-
-					callback := make(chan *bootstrapsystem.CallbackData)
-					bsSystem.Command <- &bootstrapsystem.BsCommand{
-						Command: bootstrapsystem.PROCESS_PING,
-						Data: &bootstrapsystem.ChannelData{
-							Msg:      msg,
-							Callback: callback,
-						},
-					}
-
-					select {
-					case data := <-callback:
-						if data.Success {
-							bestPop = bsSystem.GetBestPop()
-							createUI = true
-						} else {
-							panic("Error bootstraping")
-						}
-					}
-
-					// find the best pop
-					return
-				case protobuf.RequestType_RETRANSMIT:
-
-					logger.Debug("Received video chunk")
-
-					if updateUI == nil {
-            logger.Error("No UI to update")
-						return
-					}
-
-					updateUI(msg)
-
-				default:
-				}
-			}(data)
+			go handleMessage(data, cnf, bsSystem, logger, createUIChan, uiChannel)
 		}
 	}()
 
-	for {
-		if createUI {
-			createUI = false
-			updateUI = ui.StartUI(bestPop.Addr, cnf)
-      return
+	// Wait until we receive the bestPop to start the UI
+	bestPop := <-createUIChan
+
+	// Start the UI on the main thread
+	ui.StartUI(bestPop.Addr, cnf, uiChannel)
+
+	// The application will continue running until the UI is closed
+}
+
+func handleMessage(data []byte, cnf *cnf.AppConfigList, bsSystem *bootstrapsystem.BootstrapSystem, logger *config.Logger, createUIChan chan<- *bootstrapsystem.Pop, uiChannel chan<- *protobuf.Header) {
+	msg, err := config.UnmarshalHeader(data)
+	if err != nil {
+		log.Printf("Error unmarshaling message: %s\n", err)
+		return
+	}
+
+	switch msg.Type {
+	case protobuf.RequestType_CLIENT_PING:
+		if msg.Sender == "bs" {
+			logger.Debug("Received Neighbors from bootstrapper")
+			cbNb := ProcessNeighbors(cnf, msg, bsSystem)
+
+			select {
+			case result := <-cbNb:
+				if result.Success {
+					logger.Debug("Successfully received neighbors from bootstrapper")
+					ProcessBestPop(cnf, msg, bsSystem)
+					return
+				}
+				logger.Debug("Failed to receive neighbors from bootstrapper")
+			}
+			return
 		}
+
+		logger.Debug("Received POP Ping")
+		callback := make(chan *bootstrapsystem.CallbackData)
+		bsSystem.Command <- &bootstrapsystem.BsCommand{
+			Command: bootstrapsystem.PROCESS_PING,
+			Data: &bootstrapsystem.ChannelData{
+				Msg:      msg,
+				Callback: callback,
+			},
+		}
+
+		select {
+		case data := <-callback:
+			if data.Success {
+				bestPop := bsSystem.GetBestPop()
+				// Signal to create the UI if not already signaled
+				select {
+				case createUIChan <- bestPop:
+				default:
+					// Already signaled
+				}
+			} else {
+				panic("Error bootstrapping")
+			}
+		}
+	case protobuf.RequestType_RETRANSMIT:
+		logger.Debug("Received video chunk")
+		// Send the message to the UI channel
+		select {
+		case uiChannel <- msg:
+		default:
+			logger.Error("UI channel is not ready to receive messages")
+		}
+	default:
+		// Handle other message types if necessary
 	}
 }
 
 func RequestNeighbors(cnf *cnf.AppConfigList, bsAddr string, bsSystem *bootstrapsystem.BootstrapSystem) {
-
-	loop := true
-	for loop {
+	for {
 		callback := make(chan *bootstrapsystem.CallbackData)
 		bsSystem.Command <- &bootstrapsystem.BsCommand{
 			Command: bootstrapsystem.REQUEST_NEIGHBORS,
 			Data: &bootstrapsystem.ChannelData{
 				ContactAddr: bsAddr,
-				Callback:    make(chan *bootstrapsystem.CallbackData),
+				Callback:    callback,
 			},
 		}
 
 		select {
-		case _ = <-callback:
+		case <-callback:
 		}
 
 		if bsSystem.Neighbors != nil {
-			loop = false
+			break
 		}
-
 		time.Sleep(time.Second * 3)
 	}
 }
 
 func ProcessNeighbors(cnf *cnf.AppConfigList, msg *protobuf.Header, bsSystem *bootstrapsystem.BootstrapSystem) chan *bootstrapsystem.CallbackData {
 	callback := make(chan *bootstrapsystem.CallbackData)
-
 	bsSystem.Command <- &bootstrapsystem.BsCommand{
 		Command: bootstrapsystem.PROCESS_NEIGHBORS,
 		Data: &bootstrapsystem.ChannelData{
@@ -175,9 +163,7 @@ func ProcessNeighbors(cnf *cnf.AppConfigList, msg *protobuf.Header, bsSystem *bo
 }
 
 func ProcessBestPop(cnf *cnf.AppConfigList, msg *protobuf.Header, bsSystem *bootstrapsystem.BootstrapSystem) {
-
-	loop := true
-	for loop {
+	for {
 		callback := make(chan *bootstrapsystem.CallbackData)
 		bsSystem.Command <- &bootstrapsystem.BsCommand{
 			Command: bootstrapsystem.REQUEST_PING,
@@ -189,14 +175,14 @@ func ProcessBestPop(cnf *cnf.AppConfigList, msg *protobuf.Header, bsSystem *boot
 		select {
 		case data := <-callback:
 			if data.Success {
+				// Process success if needed
 			}
 		}
 
 		if bsSystem.BestPop != nil {
-			loop = false
+			break
 		}
-
 		time.Sleep(time.Second * 3)
 	}
-
 }
+
--- client/ui/ui.go
+++ client/ui/ui.go
@@ -9,109 +9,179 @@ import (
 	"sync"
 	"time"
 
-	"github.com/rodrigo0345/esr-tp2/config"
-	"github.com/rodrigo0345/esr-tp2/config/protobuf"
-	"google.golang.org/protobuf/proto"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/rodrigo0345/esr-tp2/config"
+	cnf "github.com/rodrigo0345/esr-tp2/config"
+	"github.com/rodrigo0345/esr-tp2/config/protobuf"
+	"google.golang.org/protobuf/proto"
 )
 
-func StartUI(bestPopAddr string, cnf *config.AppConfigList) func(*protobuf.Header) {
-	// Create the app and window
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Video Client")
+type Statistics struct {
+	prevDelay   float64
+	jitterSum   float64
+	packetCount int
+	totalSent   int
+	startTime   time.Time
+	mu          sync.Mutex
+}
 
-	// Create an image canvas object
-	imgCanvas := canvas.NewImageFromImage(nil)
-	imgCanvas.FillMode = canvas.ImageFillContain
+func (s *Statistics) UpdateStats(receivedTime int64, sendTimestamp int64, sequenceNumber int) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-	// Create a label to display the msg.Path
-	pathLabel := widget.NewLabel("")
-
-	// Statistics variables
-	var (
-		prevDelay   float64
-		jittersum   float64
-		packetCount int
-		startTime   = time.Now()
-		statsMutex  sync.Mutex
-		totalSent   int // Total number of packets received (sequence number)
-	)
-
-	resetStats := func() {
-		statsMutex.Lock()
-		defer statsMutex.Unlock()
-
-		// Reset statistics values
-		packetCount = 0
-		jittersum = 0.0
-		prevDelay = 0.0
+	currentDelay := float64(receivedTime - sendTimestamp)
+	if s.packetCount > 0 {
+		jitter := math.Abs(currentDelay - s.prevDelay)
+		s.jitterSum += jitter
 	}
+	s.prevDelay = currentDelay
+	s.packetCount++
+	s.totalSent = sequenceNumber
+}
 
-	// Entry widgets and buttons
-	messageEntry := widget.NewEntry()
-	messageEntry.SetPlaceHolder("Enter video")
-	targetEntry := widget.NewEntry()
-	targetEntry.SetPlaceHolder("Enter target")
-	sendButton := widget.NewButton("Send", func() {
-		sendCommand("PLAY", messageEntry.Text, targetEntry.Text, bestPopAddr, cnf, bestPopAddr, resetStats)
-	})
-	cancelButton := widget.NewButton("Stop", func() {
-		sendCommand("STOP", messageEntry.Text, targetEntry.Text, bestPopAddr, cnf, bestPopAddr, resetStats)
-	})
+func (s *Statistics) Reset() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.prevDelay = 0
+	s.jitterSum = 0
+	s.packetCount = 0
+	s.totalSent = 0
+	s.startTime = time.Now()
+}
 
-	statsButton := widget.NewButton("Statistics", func() {
-		showStatsWindow(myApp, startTime, &packetCount, &jittersum, &statsMutex, &totalSent)
-	})
+func (s *Statistics) CalculateMetrics() (float64, int, float64, float64) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-	controls := container.NewVBox(messageEntry, targetEntry, sendButton, cancelButton, statsButton, pathLabel)
-	content := container.NewBorder(nil, controls, nil, nil, imgCanvas)
+	elapsedTime := time.Since(s.startTime).Seconds()
+	avgJitter := 0.0
+	if s.packetCount > 1 {
+		avgJitter = s.jitterSum / float64(s.packetCount-1)
+	}
+	packetLoss := 0.0
+	if s.totalSent > 0 {
+		lossCount := s.totalSent - s.packetCount
+		packetLoss = float64(lossCount) / float64(s.totalSent) * 100
+	}
+	return elapsedTime, s.packetCount, avgJitter, packetLoss
+}
 
-	myWindow.SetContent(content)
-	myWindow.Resize(fyne.NewSize(800, 600)) // Adjusted size of the main window
-	myWindow.ShowAndRun()
+type UI struct {
+	imgCanvas    *canvas.Image
+	pathLabel    *widget.Label
+	messageEntry *widget.Entry
+	targetEntry  *widget.Entry
+	sendButton   *widget.Button
+	cancelButton *widget.Button
+	statsButton  *widget.Button
+	stats        *Statistics
+}
 
-	return func(msg *protobuf.Header) {
-		// update stats and ui
-		path := msg.Path
-		pathLabel.SetText(fmt.Sprintf("Path: %s", path))
-
-		videoData := msg.GetServerVideoChunk().Data
-		imgReader := bytes.NewReader(videoData)
-		frame, err := jpeg.Decode(imgReader)
-
-		if err != nil {
-			log.Printf("Error decoding JPEG: %s\n", err)
-			return
-		}
-
-		imgCanvas.Image = frame
-		imgCanvas.Refresh()
-
-		// Jitter calculation
-		receivedTime := time.Now().UnixMilli()
-		sendTimestamp := msg.GetServerVideoChunk().Timestamp
-		currentDelay := float64(receivedTime - sendTimestamp)
-
-		statsMutex.Lock()
-		if packetCount > 0 {
-			jitter := math.Abs(currentDelay - prevDelay)
-			jittersum += jitter
-		}
-		prevDelay = currentDelay
-		packetCount++
-		totalSent = int(msg.GetServerVideoChunk().GetSequenceNumber()) // Update totalSent with the sequence number
-		statsMutex.Unlock()
+func NewUI() *UI {
+	return &UI{
+		imgCanvas:    canvas.NewImageFromImage(nil),
+		pathLabel:    widget.NewLabel(""),
+		messageEntry: widget.NewEntry(),
+		targetEntry:  widget.NewEntry(),
+		sendButton:   widget.NewButton("Send", nil),
+		cancelButton: widget.NewButton("Stop", nil),
+		statsButton:  widget.NewButton("Statistics", nil),
+		stats:        &Statistics{startTime: time.Now()},
 	}
 }
 
-// Helper function to send commands
-func sendCommand(command, video, target string, bestPopAddr string, cnf *config.AppConfigList, listenIp string, resetStats func()) {
-	// Reset statistics before sending the command
-	resetStats()
+func (ui *UI) UpdateImage(data []byte) {
+	imgReader := bytes.NewReader(data)
+	frame, err := jpeg.Decode(imgReader)
+	if err != nil {
+		log.Printf("Error decoding JPEG: %s\n", err)
+		return
+	}
+	ui.imgCanvas.Image = frame
+	ui.imgCanvas.Refresh()
+}
+
+func (ui *UI) ShowStatsWindow(app fyne.App) {
+	statsWindow := app.NewWindow("Statistics")
+	statsWindow.Resize(fyne.NewSize(400, 300))
+
+	elapsedTimeLabel := widget.NewLabel("")
+	packetCountLabel := widget.NewLabel("")
+	avgJitterLabel := widget.NewLabel("")
+	packetLossRate := widget.NewLabel("")
+
+	statsContent := container.NewVBox(
+		elapsedTimeLabel,
+		packetCountLabel,
+		avgJitterLabel,
+		packetLossRate,
+	)
+	statsWindow.SetContent(statsContent)
+
+	done := make(chan struct{})
+	statsWindow.SetOnClosed(func() { close(done) })
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			default:
+				elapsedTime, packetCount, avgJitter, packetLoss := ui.stats.CalculateMetrics()
+				elapsedTimeLabel.SetText(fmt.Sprintf("Elapsed Time: %.2f seconds", elapsedTime))
+				packetCountLabel.SetText(fmt.Sprintf("Packets Received: %d", packetCount))
+				avgJitterLabel.SetText(fmt.Sprintf("Average Jitter: %.2f ms", avgJitter))
+				packetLossRate.SetText(fmt.Sprintf("Packet Loss: %.2f%%", packetLoss))
+				time.Sleep(500 * time.Millisecond)
+			}
+		}
+	}()
+	statsWindow.Show()
+}
+
+func StartUI(bestPopAddr string, cnf *cnf.AppConfigList, uiChannel <-chan *protobuf.Header) {
+	ui := NewUI()
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Streaming Client")
+
+	ui.messageEntry.SetPlaceHolder("Enter video")
+	ui.targetEntry.SetPlaceHolder("Enter target")
+	ui.sendButton.OnTapped = func() {
+		sendCommand("PLAY", ui.messageEntry.Text, ui.targetEntry.Text, bestPopAddr, cnf, ui.stats)
+	}
+	ui.cancelButton.OnTapped = func() {
+		sendCommand("STOP", ui.messageEntry.Text, ui.targetEntry.Text, bestPopAddr, cnf, ui.stats)
+	}
+	ui.statsButton.OnTapped = func() {
+		ui.ShowStatsWindow(myApp)
+	}
+
+	controls := container.NewVBox(ui.messageEntry, ui.targetEntry, ui.sendButton, ui.cancelButton, ui.statsButton, ui.pathLabel)
+	content := container.NewBorder(nil, controls, nil, nil, ui.imgCanvas)
+
+	myWindow.SetContent(content)
+	myWindow.Resize(fyne.NewSize(800, 600))
+
+	// Start a goroutine to process messages from uiChannel
+	go func() {
+		for msg := range uiChannel {
+			ui.pathLabel.SetText(fmt.Sprintf("Path: %s", msg.Path))
+			if videoChunk := msg.GetServerVideoChunk(); videoChunk != nil {
+				ui.UpdateImage(videoChunk.Data)
+				ui.stats.UpdateStats(time.Now().UnixMilli(), videoChunk.Timestamp, int(videoChunk.SequenceNumber))
+			}
+		}
+	}()
+
+	myWindow.ShowAndRun()
+}
+
+func sendCommand(command, video, target string, bestPopAddr string, cnf *cnf.AppConfigList, stats *Statistics) {
+	stats.Reset()
 
 	message := protobuf.Header{
 		Type:           protobuf.RequestType_RETRANSMIT,
@@ -134,74 +204,6 @@ func sendCommand(command, video, target string, bestPopAddr string, cnf *config.
 		log.Printf("Error marshaling message: %v", err)
 		return
 	}
-  config.SendMessageUDP(bestPopAddr, data)
-	if err != nil {
-		log.Printf("Failed to send message: %v", err)
-	}
+	config.SendMessageUDP(bestPopAddr, data)
 }
 
-func showStatsWindow(app fyne.App, startTime time.Time, packetCount *int, jittersum *float64, statsMutex *sync.Mutex, totalSent *int) {
-	statsWindow := app.NewWindow("Statistics")
-	statsWindow.Resize(fyne.NewSize(400, 300))
-
-	// Labels para exibição das estatísticas
-	elapsedTimeLabel := widget.NewLabel("")
-	packetCountLabel := widget.NewLabel("")
-	avgJitterLabel := widget.NewLabel("")
-	packetLossRate := widget.NewLabel("")
-
-	statsContent := container.NewVBox(
-		elapsedTimeLabel,
-		packetCountLabel,
-		avgJitterLabel,
-		packetLossRate,
-	)
-	statsWindow.SetContent(statsContent)
-
-	// Channel to signal window closure
-	done := make(chan struct{})
-
-	statsWindow.SetOnClosed(func() {
-		close(done)
-	})
-
-	go func() {
-		for {
-			select {
-			case <-done:
-				return // Close Goroutine when the window is close
-			default:
-				statsMutex.Lock()
-				// Elapsed time
-				elapsedTime := time.Since(startTime).Seconds()
-
-				// Calculate Avg Jitter
-				avgJitter := 0.0
-				if *packetCount > 1 {
-					avgJitter = *jittersum / float64(*packetCount-1)
-				}
-
-				// Calculate Packet Loss
-				expectedPackets := *totalSent
-				lossCount := expectedPackets - *packetCount
-
-				packetLossPercentage := 0.0
-				if expectedPackets > 0 {
-					packetLossPercentage = float64(lossCount) / float64(expectedPackets) * 100
-				}
-
-				statsMutex.Unlock()
-
-				// Update the label values
-				elapsedTimeLabel.SetText(fmt.Sprintf("Elapsed Time: %.2f seconds", elapsedTime))
-				packetCountLabel.SetText(fmt.Sprintf("Packets Received: %d", *packetCount))
-				avgJitterLabel.SetText(fmt.Sprintf("Average Jitter: %.2f ms", avgJitter))
-				packetLossRate.SetText(fmt.Sprintf("Packet Loss: %.2f%%", packetLossPercentage))
-
-				time.Sleep(500 * time.Millisecond) // Update every 500ms
-			}
-		}
-	}()
-
-	statsWindow.Show()
-}
